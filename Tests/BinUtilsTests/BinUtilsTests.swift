404681a337912fe45b898560cffec70e7dc3369c
Aidan Steele <aidan.steele@glassechidna.com.au>
2016-10-18 09:02:28 +1100

    Swift Package Manager support (#1)
    SPM expects source to be laid out in a certain way and then
    it will auto-detect all source and test files. Additional files
    were created by invoking `swift package init`. The xcodeproj
    was deleted, but a new one can be generated on-demand by
    executing `swift package generate-xcodeproj`.

--- Tests/BinUtilsTests/BinUtilsTests.swift
+++ Tests/BinUtilsTests/BinUtilsTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import BinUtils
 
 class BinUtilsTests: XCTestCase {
     
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import BinUtilsTests
+
+XCTMain([
+     testCase(BinUtilsTests.allTests),
+])
